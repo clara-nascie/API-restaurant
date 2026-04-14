6631db53e77d6e1330afff27a52db67a213ae79c
Clara Nascimento <clara.nasc13@gmail.com>
2026-04-14 15:46:54 -0300

    feat: implement products controller and routes with request validation

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,5 +1,6 @@
 //importando os tipos de requisição e resposta do express
 import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
 
 //criando a classe ProductsController para gerenciar os produtos
 class ProductsController {
@@ -12,6 +13,26 @@ class ProductsController {
         message: "Produto cadastrado",
       });
       //mostrando o erro caso ocorra na requisição 
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  //criando o método create para criar um produto
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      //criando o schema de validação do body para validar os dados que vem na requisição 
+      // o schema serve para garantir que os dados que vem na requisição estejam corretos
+      //o z.objct é usado para falar que o tipo de dado vai ser uma string ou um número
+      const bodySchema = z.object({
+        name: z.string().trim().min(6, "nome deve ter pelo menos 6 caracteres"),
+        price: z.number().gt(0, "preço deve ser maior que 0"),
+      });
+
+//usando o schema para validar os dados que vem na requisição 
+      const { name, price } = bodySchema.parse(req.body);
+
+      return res.status(201).json({ name, price });
     } catch (error) {
       next(error);
     }
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -9,5 +9,8 @@ const productsController = new ProductsController();
 //criando a rota get para listar os produtos
 productsRoutes.get("/", productsController.index);
 
+//criando a rota post para criar um produto
+productsRoutes.post("/", productsController.create);
+
 //exportando as rotas de produtos para pode usar em outros arquivos
 export { productsRoutes }
