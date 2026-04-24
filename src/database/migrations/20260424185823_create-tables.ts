da0bdb126e47a2ca445511df879db425fba55b96
Clara Nascimento <clara.nasc13@gmail.com>
2026-04-24 16:22:43 -0300

    feat: initialize database and create tables migration for restaurant tables

---
+++ src/database/migrations/20260424185823_create-tables.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+//criando o migrations para a tabela de mesas
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('tables', (table) => {
+        //colunas da tabela 
+        table.increments('id').primary();
+        table.integer("table_number").notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
+    })
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('tables');
+}
+
