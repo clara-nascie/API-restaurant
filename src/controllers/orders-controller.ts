cfc1c2f58335e5adb2f06436f871f83ad289414b
Clara Nascimento <clara.nasc13@gmail.com>
2026-05-15 17:54:55 -0300

    feat: add OrdersController and define OrderRepository type for table order management

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,6 +1,10 @@
-import {Request, Response, NextFunction } from "express";
-import { knex } from "../database/knex";
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/app-error";
+import { knex } from "@/database/knex";
 import { z } from "zod";
+import { TablesSessionsRepository } from "@/database/types/tables-sessions-repository";
+import { OrderRepository } from "@/database/types/order-repository";
+import { productRepository } from "@/database/types/product-repository";
 
 class OrdersController {
     async create (req: Request, res: Response, next: NextFunction) {
@@ -12,10 +16,32 @@ class OrdersController {
             });
 
             const { table_session_id, product_id, quantity } = bodySchema.parse(req.body);
+
+            const session = await knex<TablesSessionsRepository>("tables_sessions")
+            .where({id: table_session_id})
+            .first();
             
-            return res.status(201).json();
+            if (!session) {
+                throw new AppError("Sessão não encontrada", 404);
+            }
+
+            if(session.closed_at){
+                throw new AppError("Sessão encerrada", 400);
+            }
             
-            //await knex<OrderRepository>("products").select("*")
+            const product = await knex<productRepository>("products")
+            .where({id: product_id})
+            .first();
+            
+            if (!product) {
+                throw new AppError("Produto não encontrado", 404);
+            }
+
+
+            return res.status(201).json(product);
+
+
+            
         } catch (error) {
             next(error);
         }
